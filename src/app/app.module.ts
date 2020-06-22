84a2a72efd8a5df2c865c483ef053b548757f54c
EllyKiplaa <ellykiplangat200@gmail.com>
2020-06-23 00:44:21 +0300

    Link my formModule to my app module and import it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { ProfileComponent } from './profile/profile.component';
 import { ProfileService } from '../app/profileService';
 import { GoalFormComponent } from './goal-form/goal-form.component';
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     GoalFormComponent,
+    AboutComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
